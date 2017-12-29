c843c08d70253f7c36677dedb25ede86a8ab10f4
nprutan <nprutan@gmail.com>
2017-12-28 22:16:24 -0700

    Continued WIP on hubspot data processing. Currently working on getting refresh tokens.

--- hubspot_data_broker.py
+++ hubspot_data_broker.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def metrics_to_hubspot_payload(metrics, customer, customer_address):
     data = json.dumps([
         dict(
@@ -19,12 +20,33 @@ def metrics_to_hubspot_payload(metrics, customer, customer_address):
     ])
 
     data_json = json.loads(data)
-    for d in _expand_metrics_properties(metrics):
-        data_json[0]['properties'].append(d)
+    data_json[0]['properties'].extend([*_expand_metrics_properties(metrics)])
 
     return data_json
 
 
 def _expand_metrics_properties(metrics):
     for p in metrics.__dir__():
-        yield dict(property=p, value=metrics[p])
+        yield dict(property=p, value=str(metrics[p]))
+
+
+def post_batch_to_hubspot(payload, app_user):
+    url = 'https://api.hubapi.com/contacts/v1/contact/batch'
+    headers = {'Authorization': 'Bearer {}'.format(app_user.hs_access_token)}
+    r = requests.post(url, json=payload, headers=headers)
+    return r.json()
+
+
+# TODO: Change redirect uri /hsauth to accept tokens
+def get_new_tokens(app_user, config):
+    url = 'https://api.hubapi.com/oauth/v1/token'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+    payload = 'grant_type=refresh_token&client_id={}&client_secret={}&redirect_uri={}&refresh_token={}'.format(
+        config['HS_CLIENT_ID'],
+        config['HS_CLIENT_SECRET'],
+        config['HS_REDIRECT_URI'],
+        app_user.hs_refresh_token
+        )
+    return requests.post(url, data=payload, headers=headers).json()
+
+
